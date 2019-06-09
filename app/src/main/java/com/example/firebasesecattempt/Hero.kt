1a65a7f109581f27922356c11b8ea55f59d5574e
jahjah67 <changszehong93@gmail.com>
2019-06-09 20:55:12 +0800

    First commit

---
+++ app/src/main/java/com/example/firebasesecattempt/Hero.kt
@@ -0,0 +1,7 @@
+package com.example.firebasesecattempt
+
+class Hero(val id: String?, val name: String, val rating: Int){
+    constructor(): this("","",0){
+
+    }
+}
---
+++ app/src/main/java/com/example/firebasesecattempt/HeroAdapter.kt
@@ -0,0 +1,73 @@
+package com.example.firebasesecattempt
+
+import android.app.AlertDialog
+import android.content.Context
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.*
+import com.google.firebase.database.FirebaseDatabase
+
+class HeroAdapter(val mCtx: Context, val layoutResId: Int, val heroList: List<Hero>):ArrayAdapter<Hero>(mCtx, layoutResId, heroList){
+    override fun getView(position: Int, convertView: View?, parent: ViewGroup): View {
+        val layoutInflator : LayoutInflater = LayoutInflater.from(mCtx)
+        val view: View = layoutInflator.inflate(layoutResId, null)
+
+        val textViewName = view.findViewById<TextView>(R.id.textViewName)
+        val textViewUpdate = view.findViewById<TextView>(R.id.textViewUpdate)
+
+        val hero = heroList[position]
+
+        textViewName.text = hero.name
+        textViewUpdate.setOnClickListener{
+            showUpdateDialog(hero)
+        }
+
+        return view
+    }
+
+    private fun showUpdateDialog(hero: Hero) {
+        val builder = AlertDialog.Builder(mCtx)
+        builder.setTitle("Update Hero")
+
+        val inflater = LayoutInflater.from(mCtx)
+
+        val view = inflater.inflate(R.layout.layout_update_hero, null)
+
+        val editText = view.findViewById<EditText>(R.id.editTextName)
+        val ratingBar = view.findViewById<RatingBar>(R.id.ratingBar)
+
+        editText.setText(hero.name)
+        ratingBar.rating = hero.rating.toFloat()
+
+        builder.setView(view)
+
+        builder.setPositiveButton("Update") { dialog, which ->
+            val dbHero = FirebaseDatabase.getInstance().getReference("heroes")
+
+            val name = editText.text.toString().trim()
+
+            if(name.isEmpty()){
+                editText.error="Please enter a name"
+                editText.requestFocus()
+                return@setPositiveButton
+            }
+
+            val hero = Hero(hero.id, name, ratingBar.rating.toInt())
+
+            dbHero.child(hero.id.toString()).setValue(hero)
+
+            Toast.makeText(mCtx, "Hero Updated", Toast.LENGTH_LONG).show()
+        }
+
+        builder.setNegativeButton("Delete") { dialog, which ->
+            val myDatabase = FirebaseDatabase.getInstance().getReference("heroes")
+            myDatabase.child(hero.id.toString()).removeValue()
+        }
+
+        val alert = builder.create()
+        alert.show()
+    }
+
+
+}
